340cb058c32bca639abe2dc66e4df34893891953
Precious Bassey <precijagz480@gmail.com>
2023-05-11 19:33:49 -0700

    biography

---
+++ index.js
@@ -0,0 +1,6 @@
+const updateMe = document.querySelector("#update");
+// const change = document.querySelector(".change");
+
+updateMe.addEventListener("click", () => {
+    document.querySelector(".change").innerHTML = "I am currently learning Javascript at TIIDELab Prefellowship cohort 3.0"
+});
